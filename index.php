1c1183d7a152f2f3b0c093b31ed02b19c668d050
Mathieu Macdelayn <49252320+Macdelayn@users.noreply.github.com>
2019-11-05 23:28:38 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,23 +1,47 @@
 <?php
 
-	session_start();
-
-	$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-	use // Controllors used
-		// Requête (as Request)
-		// Réponse (as Response)
-		// Routeur (as Router)
-		//
-		//
-		//
-		//
+session_start();
 
 
-	$request = new Request;
-	$response = new Response;
-	$router = new Router($request,$response);
+require('Controllors/Router.php');
 
+try{
 
-	echo $response;
+	if (isset($_GET['action'])) {
+    	if ($_GET['action'] == 'listArticles') {
+        	listArticles();
+    	}
+        elseif ($_GET['action'] == 'article') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                article();
+            }
+            else {
+                echo 'Erreur : aucun identifiant de billet envoyé';
+                }
+        }
+        elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['userId']) && !empty($_POST['comment'])) {
+                    addComment($_GET['id'], $_POST['userId'], $_POST['comment']);
+                }
+                else {
+                    throw new Exception('Tous les champs ne sont pas remplis !');
+                }
+            }
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+        
+	}
+	else {
+    	listArticles();
+	}
+
+}
+catch(Exception $e){
+	echo 'Erreur :' .$e->getMessage();
+}	
+
 
 ?>

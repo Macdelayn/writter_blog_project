9b00c4656367645d58c680e6d1eb68f9c7e719ef
Mathieu Macdelayn <49252320+Macdelayn@users.noreply.github.com>
2019-12-17 16:46:31 +0100

    Update template.php

--- View/template.php
+++ View/template.php
@@ -1,12 +1,27 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
-        <title><?php echo $title; ?></title>
-        <link href="style.css" rel="stylesheet" /> 
+        <title><?= $title  ?></title>
+        <link href="View/style.css" rel="stylesheet" /> 
     </head>
-        
+    <header>
+        <form class="identification">
+        <div>
+            <label for="userId">Pseudo</label><br />
+            <input type="text" id="userId" name="userId" />
+        </div>
+        <div>
+            <label for="pass">mot de passe</label><br />
+            <input type="password" id="pass" name="pass">
+        </div>
+        <div>
+            <input type="submit" />
+        </div>
+        <a href="">S'inscrire</a>
+        </form>
+    </header>    
     <body>
-        <?php echo $content; ?>
+        <?= $content ?>
     </body>
 </html>

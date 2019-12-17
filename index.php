ebd58bebb0bd137bdf6016e518f78a12b48ffe6c
Mathieu Macdelayn <49252320+Macdelayn@users.noreply.github.com>
2019-12-17 16:38:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,17 +3,19 @@
 session_start();
 
 
-require('Controllors/Router.php');
+require('Controller/Controller1.php');
+
+$router = new Controller1();
 
 try{
 
 	if (isset($_GET['action'])) {
     	if ($_GET['action'] == 'listArticles') {
-        	listArticles();
+        	$router->listArticles();
     	}
-        elseif ($_GET['action'] == 'article') {
+        elseif ($_GET['action'] == 'post') {
             if (isset($_GET['id']) && $_GET['id'] > 0) {
-                article();
+                $router->article();
             }
             else {
                 echo 'Erreur : aucun identifiant de billet envoyé';
@@ -21,21 +23,21 @@ try{
         }
         elseif ($_GET['action'] == 'addComment') {
             if (isset($_GET['id']) && $_GET['id'] > 0) {
-                if (!empty($_POST['userId']) && !empty($_POST['comment'])) {
-                    addComment($_GET['id'], $_POST['userId'], $_POST['comment']);
+                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                    $router->addComment($_GET['id'], $_POST['author'], $_POST['comment']);
                 }
                 else {
                     throw new Exception('Tous les champs ne sont pas remplis !');
                 }
             }
             else {
-                throw new Exception('Aucun identifiant de billet envoyé');
+                throw new Exception('Aucun identifiezdsdant de billet envoyé');
             }
         }
         
 	}
 	else {
-    	listArticles();
+    	$router->listArticles();
 	}
 
 }

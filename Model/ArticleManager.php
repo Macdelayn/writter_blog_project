353109bc438faa36201601addc631c63cf4b0b0f
Mathieu Macdelayn <49252320+Macdelayn@users.noreply.github.com>
2019-12-17 16:47:09 +0100

    Update ArticleManager.php

--- Model/ArticleManager.php
+++ Model/ArticleManager.php
@@ -6,15 +6,18 @@ class ArticleManager extends Manager
     public function getArticles()
     {
         $bdd = $this->dbConnect();
-        $req = $bdd->query('SELECT id, title, content, DATE_FORMAT(dateArticle, \'%d/%m/%Y à %Hh%imin%ss\') AS dateArticle_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
-
-        return $req;
+        $req = $bdd->query('SELECT id, title, content, DATE_FORMAT(dateArticle, \'%d/%m/%Y à %Hh%imin%ss\') AS dateArticle_fr FROM article ORDER BY dateArticle DESC LIMIT 0, 5');
+        $req->execute();
+        
+        return $req->fetchAll();
+        
+        
     }
 
-    public function getArticle($articleId)
+    public function getArticle($postId)
     {
         $bdd = $this->dbConnect();
-        $req = $bdd->prepare('SELECT id, title, content, DATE_FORMAT(dateArticle, \'%d/%m/%Y à %Hh%imin%ss\') AS dateArticle_fr FROM posts WHERE id = ?');
+        $req = $bdd->prepare('SELECT id, title, content, DATE_FORMAT(dateArticle, \'%d/%m/%Y à %Hh%imin%ss\') AS dateArticle_fr FROM article WHERE id = ?');
         $req->execute(array($postId));
         $article = $req->fetch();
 

be7e0fec4eeba4c1e793dff7001488189d79c40f
Mathieu Macdelayn <49252320+Macdelayn@users.noreply.github.com>
2019-12-17 16:48:44 +0100

    Update Manager.php

--- Model/Manager.php
+++ Model/Manager.php
@@ -3,7 +3,11 @@
 class Manager {
 	protected function dbConnect()
 	{
-	   $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-	   return $bdd;
+        $bdd = new PDO('mysql:host=localhost;dbname=blog2;charset=utf8', 'root', '');
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $bdd->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        return $bdd;
+        
 	}
+    
 }
